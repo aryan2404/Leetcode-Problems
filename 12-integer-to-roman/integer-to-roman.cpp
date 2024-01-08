2d1d2ffb0ec616b95fdb9b8a5d9812c33ec9a154
Aryan Pandey <114234090+aryan2404@users.noreply.github.com>
2024-01-08 22:11:05 +0530

    Time: 3 ms (90.46%) | Memory: 6.3 MB (73.91%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        string ths[]={"","M","MM","MMM"};
-        
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+     string symbol[]={"M","CM","D","CD","C","XC","L","XL","X",
+        "IX","V","IV","I"};
+
+        int value[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        int i=0;
+        string ans="";
+        while(i<13){
+            while(num>=value[i]){
+                ans+=symbol[i];
+                num=num-value[i];
+            }
+            i++;
+        }
+        return ans;
     }
 };
