ae59731efdbc816530e7256aef0bc96cc627eb74
Aryan Pandey <114234090+aryan2404@users.noreply.github.com>
2024-04-04 19:42:15 +0530

    Time: 11 ms (11.26%) | Memory: 16.1 MB (48.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-  int n= nums.size();     
-int start = -1;
-int end = -1;
-if(n == 0){
-return {-1,-1};
-}
-           for(int i = 0; i < n ;i++){
-        if(nums[i] == target){
-           start = i;
-           break;
-        }
-    }
-    for(int i = n-1; i >= 0 ;i--){
-        if(nums[i] == target){
-            end = i;
-         break;
-        }
-    }
-
-    return {start,end};
+        int start = 0, end = nums.size() - 1, mid, first = -1, last = -1;
         
-}
-    
+        // Search for the first occurrence
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        // Reset start and end for the second binary search
+        start = 0;
+        end = nums.size() - 1;
+        
+        // Search for the last occurrence
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        vector<int> result(2);
+        result[0] = first;
+        result[1] = last;
+        
+        return result;
+    }
 };
