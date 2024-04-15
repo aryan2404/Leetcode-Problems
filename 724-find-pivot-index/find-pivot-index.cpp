fa9c3cadbb7474060fa2b90093367e76dab28b63
Aryan Pandey <114234090+aryan2404@users.noreply.github.com>
2024-04-15 22:36:22 +0530

    Time: 820 ms (5.22%) | Memory: 34 MB (65.82%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,35 +1,56 @@
+// #include <vector>
+
 // class Solution {
 // public:
-//     int pivotIndex(vector<int>& nums) {
-//         int n = nums.size();
-//         int start = 0;
-//         int last = n;
-//         int pivot = 
+//     int pivotIndex(std::vector<int>& nums) {
+//         if (nums.empty()) {
+//             return -1;
+//         }
+        
+//         int totalSum = 0;
+//         for (int num : nums) {
+//             totalSum += num;
+//         }
+        
+//         int leftSum = 0;
+//         for (int i = 0; i < nums.size(); ++i) {
+//             if (leftSum == totalSum - leftSum - nums[i]) {
+//                 return i;
+//             }
+//             leftSum += nums[i];
+//         }
+        
+//         return -1;
 //     }
 // };
+
 
 #include <vector>
 
 class Solution {
 public:
     int pivotIndex(std::vector<int>& nums) {
-        if (nums.empty()) {
-            return -1;
-        }
-        
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        
-        int leftSum = 0;
         for (int i = 0; i < nums.size(); ++i) {
-            if (leftSum == totalSum - leftSum - nums[i]) {
+            int leftSum = 0;
+            int rightSum = 0;
+            
+            // Calculate the sum of elements to the left of the current index
+            for (int j = 0; j < i; ++j) {
+                leftSum += nums[j];
+            }
+            
+            // Calculate the sum of elements to the right of the current index
+            for (int k = i + 1; k < nums.size(); ++k) {
+                rightSum += nums[k];
+            }
+            
+            // Check if the current index is a pivot index
+            if (leftSum == rightSum) {
                 return i;
             }
-            leftSum += nums[i];
         }
         
+        // No pivot index found
         return -1;
     }
 };
