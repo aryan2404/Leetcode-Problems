ef26267d5ecb4e714782ffd99c24a37a4cd5350f
Aryan Pandey <114234090+aryan2404@users.noreply.github.com>
2024-04-15 22:42:06 +0530

    Time: 805 ms (7.34%) | Memory: 33.9 MB (65.82%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -29,28 +29,25 @@
 
 class Solution {
 public:
-    int pivotIndex(std::vector<int>& nums) {
+    int pivotIndex(vector<int>& nums) {
         for (int i = 0; i < nums.size(); ++i) {
             int leftSum = 0;
             int rightSum = 0;
             
-            // Calculate the sum of elements to the left of the current index
             for (int j = 0; j < i; ++j) {
                 leftSum += nums[j];
             }
             
-            // Calculate the sum of elements to the right of the current index
             for (int k = i + 1; k < nums.size(); ++k) {
                 rightSum += nums[k];
             }
             
-            // Check if the current index is a pivot index
+            
             if (leftSum == rightSum) {
                 return i;
             }
         }
         
-        // No pivot index found
         return -1;
     }
 };
