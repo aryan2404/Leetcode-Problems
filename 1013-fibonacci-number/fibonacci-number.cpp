fa5a3ae5aad54845f28ddcf54cb711e9c872b306
Aryan Pandey <114234090+aryan2404@users.noreply.github.com>
2024-06-01 19:06:11 +0530

    Time: 10 ms (50.45%) | Memory: 7 MB (72.00%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 && n==1){
+          return n;  
+        }
+
+if(n>1){
+     return fib(n-1) + fib(n-2);
+}
+return n;
+
+    }
+};
