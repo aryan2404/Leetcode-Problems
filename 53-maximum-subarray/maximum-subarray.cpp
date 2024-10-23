100836fbdd7e4458916c3b0a16fd127e445ddff3
Aryan Pandey <114234090+aryan2404@users.noreply.github.com>
2024-10-23 18:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 70.6 MB (70.36%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,14 +3,8 @@ public:
     int maxSubArray(vector<int>& nums) {
          int currentSum = nums[0];
         int maxSum = nums[0];
-        
-        // Traverse the array starting from the second element
         for (int i = 1; i < nums.size(); i++) {
-            // Choose to either add the current element to the existing subarray
-            // or start a new subarray at nums[i]
-            currentSum = max(nums[i], currentSum + nums[i]);
-            
-            // Update the maximum sum found so far
+          currentSum = max(nums[i], currentSum + nums[i]);
             maxSum = max(maxSum, currentSum);
         }
         
